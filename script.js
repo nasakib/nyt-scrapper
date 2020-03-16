e45755600c4bc44720c79ec141a7340443147244
nsakib <nat.sakib@gmail.com>
2020-03-16 13:40:57 -0400

    Added script.js file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 $("button").on("click", function() {
-    var animal = $(this).attr("data-animal");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+    var searchTerms = $(this).attr("terms");
+    var queryURL = "" +
+      terms + "";
